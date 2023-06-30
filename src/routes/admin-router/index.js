fec14ed50fd2d359183bf1b033003b5921577ffa
EreZAzariyA <erezazariya@gmail.com>
2023-06-30 13:26:05 +0300

    Admin route fix

--- src/routes/admin-router/index.js
+++ src/routes/admin-router/index.js
@@ -5,9 +5,7 @@ import { AdminDashboard } from "../../components/admin/adminDashboard";
 import { CategoriesList } from "../../components/admin/categories-list";
 import { AdminInsert } from "../../components/admin/insertDoc";
 import { Provider } from "react-redux";
-import { Login } from "../../layout/AuthView/login";
 import { AuthView } from "../../layout/AuthView";
-import { Register } from "../../layout/AuthView/register";
 import { AdminRoute } from "../AdminRoute";
 import { PageNotFound } from "../../components/PageNotFound";
 import store from "../../redux/store";
@@ -32,15 +30,7 @@ export const AdminRouter = () => (
           <Route path="*" element={<Navigate to={'page-not-found'} replace />} />
         </Route>
 
-        <Route path="/auth" element={<AuthView />}>
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
-          <Route path="page-not-found" element={<PageNotFound />} />
-
-          <Route path="/auth" element={<Navigate to={'login'} />} />
-          <Route path="*" element={<Navigate to={'page-not-found'} />} />
-        </Route>
-
+        <Route path="auth/*" element={<AuthView />} />
         <Route path="*" element={<Navigate to={'/auth/login'} />} />
       </Routes>
     </Provider>
