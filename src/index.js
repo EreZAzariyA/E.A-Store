09d2b2ce68472e36b233af15f4b1001ca5cc88da
EreZAzariyA <erezazariya@gmail.com>
2023-08-07 20:42:56 +0300

    admin-side work

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
-import AdminRouter from './routes/AdminRouter';
-import UserRouter from './routes/UserRouter';
+// import AdminRouter from './routes/AdminRouter';
+// import UserRouter from './routes/UserRouter';
 import { interceptorsService } from './services/InterceptorsService';
 import './index.css';
 import './styles/global.css';
@@ -14,9 +14,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 const isAdmin = JSON.parse(process.env.REACT_APP_IS_ADMIN);
 
 if (isAdmin) {
-  root.render(<AdminRouter />);
-} else {
-  root.render(<UserRouter />);
+  import("./routes/AdminRouter").then(( AdminRouter ) => {
+    root.render(AdminRouter.default())
+  });
 }
 
 reportWebVitals();
