f692285a529a2e90be648bd1ed74492d257668a8
EreZAzariyA <erezazariya@gmail.com>
2023-08-12 11:53:47 +0300

    socket middleware removed

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,10 @@
 import {configureStore, getDefaultMiddleware} from "@reduxjs/toolkit";
-import { socketServices } from "../services/socket-services";
 import authSlicer from "./slicers/auth-slicer"
 import cartSlicer from "./slicers/cart-slicer";
 import productsSlicer from "./slicers/products-slicer";
 import categoriesSlicer from "./slicers/categories-slicer";
 import subCategoriesSlicer from "./slicers/subCategories-slicer";
 import { middleware } from "./middlewares/middleware";
-import { socketMiddleware } from "./middlewares/socketMiddleware";
 
 const store = configureStore({
   reducer: {
@@ -16,7 +14,7 @@ const store = configureStore({
     categories: categoriesSlicer.reducer,
     subCategories: subCategoriesSlicer.reducer,
   },
-  middleware: [...getDefaultMiddleware(), middleware, socketMiddleware(socketServices.socketIo)],
+  middleware: [...getDefaultMiddleware(), middleware],
 });
 
 export default store;
